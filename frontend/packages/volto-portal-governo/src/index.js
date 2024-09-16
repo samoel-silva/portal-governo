75903e15fe7a320a1a83a0e83e38c3cb13f7f369
samoel-silva <samoel-silva@procergs.rs.gov.br>
2024-09-16 10:13:53 -0300

    Exercicio 19: altera a visao do tipo Secretaria

--- frontend/packages/volto-portal-governo/src/index.js
+++ frontend/packages/volto-portal-governo/src/index.js
@@ -1,9 +1,17 @@
+// Views
+import SecretariaView from './components/Views/SecretariaView';
+
 const applyConfig = (config) => {
   config.settings = {
     ...config.settings,
     isMultilingual: false,
     supportedLanguages: ['pt-br'],
     defaultLanguage: 'pt-br',
+  };
+  // Views
+  config.views.contentTypesViews = {
+    ...config.views.contentTypesViews,
+    Secretaria: SecretariaView,
   };
   return config;
 };
